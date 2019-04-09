4ab2a2899bc9a07b878c087917c8cc44d45c3ef4
Gopal <GRADHAK3@ford.com>
2019-04-09 11:59:10 +0530

    bk

--- src/UI/canvasContext.js
+++ src/UI/canvasContext.js
@@ -3,4 +3,13 @@ export default class canvasEle {
     this.canvasTag = document.createElement("canvas");
     document.body.appendChild(this.canvasTag);
   }
+
+  getContext() {
+    try {
+      const context = this.canvasTag.getContext("2d");
+      return context;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
 }
--- src/js/App.js
+++ src/js/App.js
@@ -1,34 +1,31 @@
-import canvasEle from "../UI/canvasEle";
+import canvasEle from "../UI/canvasContext";
 
 export default class App extends canvasEle {
   constructor() {
     super();
   }
 
-  getContext() {
-    try {
-      const context = this.canvasTag.getContext("2d");
-      return context;
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  rectangle(width, height) {
+  baseRectangle(width, height) {
     const renderingContext2D = new Promise((resolve, reject) => {
       resolve(this.getContext());
       reject(this.getContext());
     });
     renderingContext2D
       .then(ctx2D => {
-        ctx2D.rect(10, 20, width, height);
+        ctx2D.beginPath();
+        ctx2D.strokeRect(10, 20, width, height);
+        ctx2D.strokeStyle = "#000";
+        ctx2D.fillStyle = "#fff";
         ctx2D.fill();
+        ctx2D.closePath();
       })
       .catch(error => {
         console.log(error);
       });
   }
+
+  line() {}
 }
 
 const app = new App();
-app.rectangle(100, 100);
+app.baseRectangle(100, 100);
